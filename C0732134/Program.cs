92da0c766c818654fd9cbd68bc58524cfd5a61f5
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-29 19:46:12 -0400

    7:45pm

--- C0732134/Program.cs
+++ C0732134/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace C0732134
 {
-    class Program
+    public class Prog
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            ToDownload();
+            
+            Console.ReadLine();
         }
+
+        public static void ToDownload()
+        {
+            Thread.Sleep(3000);
+        }
+    }
+
+    public class HelloWorld
+    {
+
+        public void sayHello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+       
     }
 }
